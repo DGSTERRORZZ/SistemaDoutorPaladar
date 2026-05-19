aacd66deb725a42831857893a621b799d67ad4d1
VALMIR PEREIRA DA ROSA FILHO <CJ3027678@ifspcjoad.ifspcjo.edu.br>
2026-05-19 08:43:39 -0300

    Samuel: toast, debounce PDV, loading, empty-state, responsividade tabelas

---
+++ backend/adicionar_imagens.js
@@ -0,0 +1,25 @@
+﻿const db = require('./database');
+(async () => {
+    const d = await db.getDatabase();
+    
+    try {
+        d.run('ALTER TABLE produtos ADD COLUMN imagem TEXT DEFAULT ""');
+        console.log('Coluna imagem adicionada');
+    } catch (e) {
+        console.log('Coluna imagem já existe');
+    }
+    
+    const atualizar = d.prepare('UPDATE produtos SET imagem = ? WHERE categoria = ? AND (imagem IS NULL OR imagem = "")');
+    
+    atualizar.run(['🥟', 'Salgados']);
+    atualizar.run(['🥤', 'Bebidas']);
+    atualizar.run(['🍰', 'Doces']);
+    atualizar.run(['🥪', 'Lanches']);
+    atualizar.run(['🍦', 'Sorvetes']);
+    atualizar.run(['🥗', 'Saudáveis']);
+    atualizar.run(['🍽️', 'Refeições']);
+    atualizar.free();
+    
+    db.saveDatabase();
+    console.log('Imagens adicionadas aos produtos!');
+})();
