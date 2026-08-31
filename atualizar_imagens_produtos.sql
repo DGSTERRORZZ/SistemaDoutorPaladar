47d15dbba878ea727ddebbd2f5245e36bdd3ea2a
VALMIR PEREIRA DA ROSA FILHO <CJ3027678@ifspcjoad.ifspcjo.edu.br>
2026-08-31 12:06:18 -0300

    feat: suporte completo para caminhos de imagens locais de produtos (frontend/imagens/) e script SQL executado para os 48 produtos

---
+++ atualizar_imagens_produtos.sql
@@ -0,0 +1,64 @@
+-- =========================================================================
+-- ATUALIZAÇÃO DOS CAMINHOS DE IMAGENS LOCAIS (frontend/imagens/)
+-- Banco: doutor_paladar | Tabela: produtos (48 Itens)
+-- =========================================================================
+
+-- Salgados
+UPDATE produtos SET imagem = 'imagens/hamburgao_cheddar.jpg' WHERE id = 1;
+UPDATE produtos SET imagem = 'imagens/hamburgao_bacon.jpg' WHERE id = 2;
+UPDATE produtos SET imagem = 'imagens/coxinha_frango.jpg' WHERE id = 3;
+UPDATE produtos SET imagem = 'imagens/pastel_carne.jpg' WHERE id = 4;
+UPDATE produtos SET imagem = 'imagens/risolis_queijo.jpg' WHERE id = 5;
+UPDATE produtos SET imagem = 'imagens/risolis_carne.jpg' WHERE id = 6;
+UPDATE produtos SET imagem = 'imagens/esfiha_carne.jpg' WHERE id = 7;
+UPDATE produtos SET imagem = 'imagens/pao_de_queijo_90g.jpg' WHERE id = 8;
+UPDATE produtos SET imagem = 'imagens/pao_batata_frango.jpg' WHERE id = 9;
+UPDATE produtos SET imagem = 'imagens/pao_batata_calabresa.jpg' WHERE id = 10;
+UPDATE produtos SET imagem = 'imagens/enroladinho_bauru.jpg' WHERE id = 11;
+UPDATE produtos SET imagem = 'imagens/enroladinho_salsicha.jpg' WHERE id = 12;
+UPDATE produtos SET imagem = 'imagens/pizza_fria.jpg' WHERE id = 13;
+UPDATE produtos SET imagem = 'imagens/kibe.jpg' WHERE id = 14;
+
+-- Bebidas
+UPDATE produtos SET imagem = 'imagens/coca_cola_lata_350ml.jpg' WHERE id = 15;
+UPDATE produtos SET imagem = 'imagens/coca_cola_pet_600ml.jpg' WHERE id = 16;
+UPDATE produtos SET imagem = 'imagens/coca_cola_zero_350ml.jpg' WHERE id = 17;
+UPDATE produtos SET imagem = 'imagens/coca_cola_zero_200ml.jpg' WHERE id = 18;
+UPDATE produtos SET imagem = 'imagens/sprite_lata_350ml.jpg' WHERE id = 19;
+UPDATE produtos SET imagem = 'imagens/guarana_antarctica_zero_lata.jpg' WHERE id = 20;
+UPDATE produtos SET imagem = 'imagens/agua_passa_quatro_com_gas.jpg' WHERE id = 21;
+UPDATE produtos SET imagem = 'imagens/agua_passa_quatro_sem_gas.jpg' WHERE id = 22;
+UPDATE produtos SET imagem = 'imagens/suco_kmais_integral_300ml.jpg' WHERE id = 23;
+UPDATE produtos SET imagem = 'imagens/suco_kmais_uva_300ml.jpg' WHERE id = 24;
+UPDATE produtos SET imagem = 'imagens/suco_kmais_goiaba_300ml.jpg' WHERE id = 25;
+UPDATE produtos SET imagem = 'imagens/suco_kmais_caju_300ml.jpg' WHERE id = 26;
+UPDATE produtos SET imagem = 'imagens/cafe_coado.jpg' WHERE id = 27;
+
+-- Doces
+UPDATE produtos SET imagem = 'imagens/cremosinho_sorvete_iogurte.jpg' WHERE id = 28;
+UPDATE produtos SET imagem = 'imagens/geladao_acai.jpg' WHERE id = 29;
+UPDATE produtos SET imagem = 'imagens/trento_chocolate.jpg' WHERE id = 30;
+UPDATE produtos SET imagem = 'imagens/trento_branco.jpg' WHERE id = 31;
+UPDATE produtos SET imagem = 'imagens/pao_de_mel_50g.jpg' WHERE id = 32;
+UPDATE produtos SET imagem = 'imagens/brigadeiro.jpg' WHERE id = 33;
+
+-- Caldos
+UPDATE produtos SET imagem = 'imagens/caldo_sopa_legumes.jpg' WHERE id = 34;
+UPDATE produtos SET imagem = 'imagens/caldo_mandioquinha_frango.jpg' WHERE id = 35;
+UPDATE produtos SET imagem = 'imagens/caldo_abobora.jpg' WHERE id = 36;
+UPDATE produtos SET imagem = 'imagens/caldo_verde_carne.jpg' WHERE id = 37;
+UPDATE produtos SET imagem = 'imagens/caldo_vaca_atolada.jpg' WHERE id = 38;
+
+-- Snacks
+UPDATE produtos SET imagem = 'imagens/halls_melancia.jpg' WHERE id = 39;
+UPDATE produtos SET imagem = 'imagens/halls_morango.jpg' WHERE id = 40;
+UPDATE produtos SET imagem = 'imagens/halls_menta.jpg' WHERE id = 41;
+UPDATE produtos SET imagem = 'imagens/trident_hortela.jpg' WHERE id = 42;
+UPDATE produtos SET imagem = 'imagens/trident_morango.jpg' WHERE id = 43;
+UPDATE produtos SET imagem = 'imagens/trident_tutti_frutti.jpg' WHERE id = 44;
+
+-- Cremosinho
+UPDATE produtos SET imagem = 'imagens/cremosinho_abacaxi.jpg' WHERE id = 45;
+UPDATE produtos SET imagem = 'imagens/cremosinho_maracuja.jpg' WHERE id = 46;
+UPDATE produtos SET imagem = 'imagens/cremosinho_morango.jpg' WHERE id = 47;
+UPDATE produtos SET imagem = 'imagens/acai_no_copo.jpg' WHERE id = 48;
